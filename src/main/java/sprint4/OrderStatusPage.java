785b21ac43a9f4b6ab25aae7ccb0a2b2274340bf
Yuri Pankrushin <pankrushinyuri@mail.ru>
2022-12-27 23:29:48 +0300

    Remove commented code in WrongOrderStatusTest

--- src/main/java/sprint4/OrderStatusPage.java
+++ src/main/java/sprint4/OrderStatusPage.java
@@ -3,8 +3,6 @@ package sprint4;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
 
@@ -20,8 +18,6 @@ public class OrderStatusPage extends AbstractPage {
     private final By orderNotFound = By.xpath(".//img[@alt='Not found']");
 
     public List<WebElement> returnOrderNotFoundPic() {
-//        new WebDriverWait(driver, 3)
-//                .until(ExpectedConditions.visibilityOfElementLocated(orderNotFound));
         return driver.findElements(orderNotFound);
     }
 }
